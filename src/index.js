f5e38ce8f97a6e8826134731b0859b92dfa03e13
Hannah McCullough <hmac091@gmail.com>
2021-12-02 18:37:44 -0700

    Completed Again - Some Advanced

--- src/index.js
+++ src/index.js
@@ -1,71 +1,95 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // create variable for desired element
-  const form = document.querySelector('form')
-  // add event listener to form
-  form.addEventListener('submit', (e) => {
-    // prevent default action for submit action 
-    e.preventDefault();
-    // pass callback function to modify to-do list
-    buildToDo(e.target.new_task_description.value);
-    // reset form input field after each task is created
-    form.reset();
-  })
+// ! CORE DELIVERABLES
+// - type task into the input field - DONE
+// - click some form of a submit button - DONE
+// - see the task that was inputted in the DOM after submission - DONE
+
+// ! ADVANCED DELIVERABLES
+// - a delete dunction to remove tasks from list - DONE
+// - select priority dropdown menu for each task - changes text color
+// - additional input field -> user, duration, date due
+// - ability to edit tasks
+// - something crazzzaayyyy 
+
+// ! GLOBAL VARIABLES
+
+// find form 
+const form = document.querySelector('#create-task-form');
+// find input value
+const userInput = document.querySelector('#new-task-description');
+// find to do list
+const todoList = document.querySelector('#tasks');
+
+document.addEventListener('DOMContentLoaded', () => {
+  form.addEventListener('submit', handleSubmitTodo);
 });
 
-function buildToDo(toDo) {
-  // create new element from input 
-  let li = document.createElement('li');
-  // create delete button for new task
-  let btn = document.createElement('button');
+// ! MAIN HANDLE FUNCTIONS
+function handleSubmitTodo(e) {
+  // prevent submit default action
+  e.preventDefault();
+  // create variable equal to input values 
+  const newTask = userInput.value;
 
-  // create dropdown for priority selection
-  let priority = document.createElement('select');
-  // create dropdown options and content
-  let selectPlaceholder = document.createElement('option');
-  selectPlaceholder.textContent = 'Choose Task Priority';
-  let highPriority = document.createElement('option');
-  highPriority.textContent = 'Red - High';
-  let medPriority = document.createElement('option');
-  medPriority.textContent = 'Yellow - Medium';
-  let lowPriority = document.createElement('option');
-  lowPriority.textContent = 'Green - Low';
+  // create new li element
+  const taskEl = document.createElement('li')
+  // create new button element
+  const btn = document.createElement('button');
+  // create priority dropdown
+  const priorityDrop = document.createElement('select');
+  // create priority options
+  const priorityOptDefault = document.createElement('option');
+  const priorityOpt1 = document.createElement('option');
+  const priorityOpt2 = document.createElement('option');
+  const priorityOpt3 = document.createElement('option');
 
-  // append options to dropwdown
-  priority.appendChild(selectPlaceholder);
-  priority.appendChild(highPriority);
-  priority.appendChild(medPriority);
-  priority.appendChild(lowPriority);
+  // set taskEl text to newTask
+  taskEl.innerText = newTask;
+  // set btn text
+  btn.innerText = 'REMOVE';
+  // set Dropdown text
+  priorityDrop.innerText = 'Test';
+  // set priority options text
+  priorityOptDefault.innerText = 'Choose a priority level';
+  priorityOpt1.innerText = 'High Priority';
+  priorityOpt2.innerText = 'Medium Priority';
+  priorityOpt3.innerText = 'Low Priority';
 
-  // add event listener to delete button
-  btn.addEventListener('click', handleDelete);
-  // assign button content
-  btn.textContent = ' x ';
-
-  // assign content to li
-  li.textContent = `${toDo}  `
-  // append button to new task
-  li.appendChild(btn);
-  // append dropdown to new task
-  li.appendChild(priority);
-
+  // add event listener to button
+  btn.addEventListener('click', handleRemove);
   // add event listener to dropdown
-  priority.addEventListener('change', handlePriority);
+  priorityDrop.addEventListener('change', handleOption);
 
-  // select element to add new tasks to
-  document.querySelector('#tasks').appendChild(li);
+  // append taskEl to list
+  todoList.appendChild(taskEl)
+  // append priority dropdown to taskEl
+  taskEl.append(priorityDrop);
+  // append options to dropdown
+  priorityDrop.appendChild(priorityOptDefault);
+  priorityDrop.appendChild(priorityOpt1);
+  priorityDrop.appendChild(priorityOpt2);
+  priorityDrop.appendChild(priorityOpt3);
+  // append delete btn to li
+  taskEl.appendChild(btn);
+
+  // reset form 
+  form.reset();
 }
 
-function handleDelete(e) {
+// ! HELPER HANDLE FUNCTIONS
+function handleRemove(e) {
+  // prevent default action
+  e.preventDefault();
+  // remove parent li from list 
   e.target.parentNode.remove();
 }
 
-function handlePriority(e) {
-  console.log(e.target.value)
-  if (e.target.value === 'Red - High') {
+function handleOption(e) {
+  let optionValue = e.target.value;
+  if (optionValue === 'High Priority') {
     e.target.parentNode.style.color = 'red';
-  } else if (e.target.value === 'Yellow - Medium') {
+  } else if (optionValue === 'Medium Priority') {
     e.target.parentNode.style.color = 'yellow';
-  } else if (e.target.value = 'Green - Low' ) {
+  } else if (optionValue === 'Low Priority') {
     e.target.parentNode.style.color = 'green';
   }
 }
